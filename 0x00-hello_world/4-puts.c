5e7699adcb72a1bb98a6f2dffde9eda80b2be010
kimkelvin7 <kelvinkimm70@gmail.com>
2023-09-09 00:56:58 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main -it prints out output using put
  *
  * Return: always (0)
- * 
+ *
  */
 int main(void)
 {
-	puts("\"programming is like building a multilinguail puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
